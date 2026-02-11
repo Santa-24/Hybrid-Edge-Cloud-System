bab90d933cf4bcfbb83674bc67740adeaf0f93ef
unknown <shubhashreerout116@gmail.com>
2026-02-11 15:29:46 +0530

    file uploading

--- cloud-layer/mqtt-communication/mqtt_config.py
+++ cloud-layer/mqtt-communication/mqtt_config.py
@@ -1,7 +1,8 @@
 # mqtt_config.py
 
-BROKER = "broker.hivemq.com"      # Public MQTT broker
+BROKER = "broker.hivemq.com"
 PORT = 1883
-TOPIC = "hybrid_edge_cloud/risk"  # Topic where edge publishes risk
+TOPIC = "hybrid_edge_cloud/risk"
 CLIENT_ID = "cloud-subscriber-01"
 KEEP_ALIVE = 60
+QOS_LEVEL = 1
